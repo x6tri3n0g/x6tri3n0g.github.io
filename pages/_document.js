6c7541ea92b58bec8c09b87f6777d6f96990c42d
xtring <hh940630@gmail.com>
2024-11-16 03:20:25 +0900

    init: init setting

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
-import {Html, Head, Main, NextScript} from 'next/document'
+import { Html, Head, Main, NextScript, Link } from "next/document";
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
-                    rel="stylesheet"/>
-            </Head>
-            <body>
-            <Main/>
-            <NextScript/>
-            </body>
-        </Html>
-    )
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Noto+Sans+KR:wght@100..900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
